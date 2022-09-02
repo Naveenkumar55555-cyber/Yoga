b3ccaa4c2bb4c73e53789bdc1137216670fcb37e
Yoga SEWA <108191442+yoga-sewa@users.noreply.github.com>
2022-09-02 22:53:12 +0530

    mail server change

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,38 +1,14 @@
-var form = document.getElementById("contact-form");
+function sendMail(params) {
+    var tempParams = {
+        from_name: document.getElementById('_name').value,
+        to_name: "Team",
+        message: `${document.getElementById('_mail').value}
+        
+        ${document.getElementById('_message').value}`,
+    };
 
-async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("contact-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            status.innerHTML = "Submitted successfully!";
-            status.classList.remove('uk-text-danger');
-            status.classList.add('uk-text-success');
-            window.location.reload();
-        } else {
-            response.json().then(data => {
-                if (Object.hasOwn(data, 'errors')) {
-                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-                } else {
-                    status.innerHTML = "Oops! Try Again!";
-                    status.classList.add('uk-text-danger');
-                    status.classList.remove('uk-text-success');
-                    window.location.reload();
-                }
-            })
-        }
-    }).catch(error => {
-        status.innerHTML = "Oops! Try Again!";
-        status.classList.add('uk-text-danger');
-        status.classList.remove('uk-text-success');
-        window.location.reload();
-    });
+    emailjs.send('gmail', 'template_30dv6mg', tempParams)
+        .then(function(res) {
+            console.log(res.status);
+        });
 }
-form.addEventListener("submit", handleSubmit);
