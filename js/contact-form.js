bff1159608272962c36de3113d178222bd1dd72d
Yoga SEWA <108191442+yoga-sewa@users.noreply.github.com>
2022-09-05 11:01:38 +0530

    contact

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,14 +1,19 @@
-function sendMail(params) {
-    var tempParams = {
-        from_name: document.getElementById('_name').value,
-        to_name: "Team",
-        message: `${document.getElementById('_mail').value}
-        
-        ${document.getElementById('_message').value}`,
-    };
+function sendMail() {
+    Email.send({
+        SecureToken : "9b2898ee-d697-43bd-bafc-8298ee2c3011",
+        To : 'yogasewa3@gmail.com',
+        From : document.getElementById('_mail').value,
+        Subject : "This is the subject",
+        Body : `Name: ${document.getElementById('_name').value} <br>
+                Email: ${document.getElementById('_mail').value} <br>
+                Message: ${document.getElementById('_message').value}`
+    }).then(
+      message => alert(message)
+    );
+}
 
-    emailjs.send('gmail', 'template_30dv6mg', tempParams)
-        .then(function(res) {
-            console.log(res.status);
-        });
+function reset() {
+    document.getElementById('_name').value = '';
+    document.getElementById('_mail').value = '';
+    document.getElementById('_message').value = '';
 }
