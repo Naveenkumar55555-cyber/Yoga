0147b4926046d250a16c59e9a2dd8e3162d62f63
Yoga SEWA <108191442+yoga-sewa@users.noreply.github.com>
2022-09-17 17:54:00 +0530

    log rem

--- js/content.js
+++ js/content.js
@@ -17,7 +17,6 @@ select.addEventListener('change', () => {
     axios.get('https://yoga-sewa-api.herokuapp.com/asanas')
         .then((data) => {
             data = data.data;
-            console.log(data);
             document.getElementById('name').innerText = data[select.selectedIndex - 1].name.en;
             document.getElementById('body').innerText = data[select.selectedIndex - 1].body;
             document.getElementById('img').innerHTML = `<img alt="${data[select.selectedIndex - 1].name.en}" src="${data[select.selectedIndex - 1].img}"  width="300" height="300">`;
