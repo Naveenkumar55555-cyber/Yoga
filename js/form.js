622a3f9a8f36532f57d77a486262e2fc5fe402d4
Kishan <kishansreddy05@gmail.com>
2022-09-18 19:12:28 +0530

    fmt

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 function sendMail() {
     Email.send({
-        SecureToken : "9b2898ee-d697-43bd-bafc-8298ee2c3011",
-        To : 'yogasewa3@gmail.com',
-        From : 'yogasewa3@gmail.com',
-        Subject : "New Submission on Contact Form",
-        Body : `Name: ${document.getElementById('_name').value} <br>
+        SecureToken: "9b2898ee-d697-43bd-bafc-8298ee2c3011",
+        To: 'yogasewa3@gmail.com',
+        From: 'yogasewa3@gmail.com',
+        Subject: "New Submission on Contact Form",
+        Body: `Name: ${document.getElementById('_name').value} <br>
                 Email: ${document.getElementById('_mail').value} <br>
                 Message: ${document.getElementById('_msg').value}`
     }).then(
