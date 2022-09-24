9c5c80d05d93b77483e53d7ed002b7178c79da8a
Kishan S Reddy <108191442+yoga-sewa@users.noreply.github.com>
2022-09-24 15:32:32 +0530

    url update

--- js/content.js
+++ js/content.js
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 select.addEventListener('change', () => {
-    axios.get('https://yogasewaapi.herokuapp.com/asanas')
+    axios.get(`https://yogasewaapi.herokuapp.com/asanas/${select.selectedIndex - 1}`)
         .then((data) => {
             data = data.data;
-            document.getElementById('name').innerText = data[select.selectedIndex - 1].name.en;
-            document.getElementById('body').innerText = data[select.selectedIndex - 1].body;
-            document.getElementById('img').innerHTML = `<img alt="${data[select.selectedIndex - 1].name.en}" src="${data[select.selectedIndex - 1].img}"  width="300" height="300">`;
+            document.getElementById('name').innerText = data.name.en;
+            document.getElementById('body').innerText = data.body;
+            document.getElementById('img').innerHTML = `<img alt="${data.name.en}" src="${data.img}"  width="300" height="300">`;
         });
 });
